122d8e212ef96b7cf1a9e5edd8a867b074c47172
VinceTamayo <vincevicetamayo@gmail.com>
2022-07-30 22:19:11 +0800

    some fix :)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 export default function Card(props) {
     return (
@@ -7,12 +9,17 @@ export default function Card(props) {
                 <img src={props.img} width="125" height="168" />
             </div>
             <div className="card-text">
-                <h3>{props.title}</h3>
-                <h5>{props.location}</h5>
-                <h5>{props.maps}</h5>
-                <h5>{props.startDate}</h5>
-                <h5>{props.endDate}</h5>
-                <h5>{props.description}</h5>
+                <div className="card-location">
+                    <FontAwesomeIcon icon={faLocationDot} className="fontawesome-location" />
+                    <h5 className="text-location">{props.location}</h5>
+                    <a href={props.maps} target="_blank" className="maps-link">View on Google Maps</a>
+                </div>
+                <h3 className="card-title">{props.title}</h3>
+                <div className="card-dates">
+                    <h5>{props.startDate} -</h5>
+                    <h5>{props.endDate}</h5>
+                </div>
+                <p className="card-desc">{props.description}</p>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faEarthAsia } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar(){
     return (
         <nav>
-            <FontAwesomeIcon icon={faGlobe} />
+            <FontAwesomeIcon icon={faEarthAsia} />
             <h4>My Travel Journal</h4>
         </nav>
     )
